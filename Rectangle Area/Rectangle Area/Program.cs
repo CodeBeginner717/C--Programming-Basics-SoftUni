2d77b0cbf4117266882fff55f7f85183095bcdd9
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-05-22 01:26:44 +0300

    Add files via upload

---
+++ Rectangle Area/Rectangle Area/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace HelloSoftUni
+{
+    class Rectangle_Area
+    {
+        static void Main(string[] args)
+        {
+
+            int a = int.Parse(Console.ReadLine());
+            int b = int.Parse(Console.ReadLine());
+
+            int area = a * b;
+            Console.WriteLine(area);
+
+
+        }
+    }
+}
