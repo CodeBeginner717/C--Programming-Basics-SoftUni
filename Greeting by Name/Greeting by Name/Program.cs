f0a40a190434f6f71b8f31adf8e446243962f412
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-05-23 22:09:49 +0300

    Add files via upload

---
+++ Greeting by Name/Greeting by Name/Program.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+class Greeting_by_Name
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+        Console.WriteLine("Hello, " + name + "!");
+    }
+}
