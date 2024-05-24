d7d85445bbd4f73bd27c9ac6ab2043f1f1cebf0b
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-05-24 19:51:37 +0300

    Add files via upload

---
+++ Yard Greening/Yard Greening/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Yard_Greening
+{
+    static void Main(string[] args)
+    {
+        double greenArea = double.Parse(Console.ReadLine());
+
+        double price = greenArea * 7.61;
+        double discount = price * 0.18;
+        double finalPrice = price - discount;
+
+        Console.WriteLine($"The final price is: {finalPrice} lv.");
+        Console.WriteLine($"The discount is: {discount} lv.");
+    }
+}
