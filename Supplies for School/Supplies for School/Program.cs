2818904ce0401d216c3fa81c3871f5e34cd82927
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-05-26 20:50:44 +0300

    Add files via upload

---
+++ Supplies for School/Supplies for School/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Sipplies_for_School
+{
+    static void Main(String[] args)
+    {
+        int packagesNumberOfPens = int.Parse(Console.ReadLine());
+        int packagesNumberOfMarkers = int.Parse(Console.ReadLine());
+        int LitersOfDetergent = int.Parse(Console.ReadLine());
+        int percentageDiscount = int.Parse(Console.ReadLine());
+
+        double detergentPerLiterPrice = LitersOfDetergent * 1.20;
+        double markersPrice = packagesNumberOfMarkers * 7.20;
+        double pensPrice = packagesNumberOfPens * 5.80;
+
+        double priceOfEverything = detergentPerLiterPrice + markersPrice + pensPrice;
+
+        double priceWithDiscount = priceOfEverything - (priceOfEverything * ((double)percentageDiscount / 100));
+
+        Console.WriteLine(priceWithDiscount);
+
+    }
+}
---
+++ Vacation Books List/Vacation Books List/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Vacation_Book_List
+{
+    static void Main(String[] args)
+    {
+
+        int numberOfPages = int.Parse(Console.ReadLine());
+        int pagesForHour = int.Parse(Console.ReadLine());
+        int numberOfDays = int.Parse(Console.ReadLine());
+
+        int numberOfHours = (numberOfPages / pagesForHour) / numberOfDays;
+
+        Console.WriteLine(numberOfHours);
+    }
+}
