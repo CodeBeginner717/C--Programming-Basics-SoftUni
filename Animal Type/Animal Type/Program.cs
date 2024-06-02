4d8d1d8a9ac4502d5b03206ac058b8aa98b51814
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-02 14:15:32 +0300

    Add files via upload

---
+++ Animal Type/Animal Type/Program.cs
@@ -0,0 +1,22 @@
+﻿class Animal_Type
+{
+    static void Main(string[] args)
+    {
+        string animal = Console.ReadLine();
+
+        switch (animal)
+        {
+            case "dog":
+                Console.WriteLine("mammal");
+                break;
+            case "crocodile":
+            case "tortoise":
+            case "snake":
+                Console.WriteLine("reptile");
+                break;
+            default:
+                Console.WriteLine("unknown");
+                break;
+        }
+    }
+}
