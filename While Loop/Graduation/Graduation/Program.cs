16f83da3aec49343d9bf6b97b47a7f63edb65f8a
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 20:22:26 +0300

    Add files via upload

---
+++ While Loop/Graduation/Graduation/Program.cs
@@ -0,0 +1,27 @@
+﻿class Graduation
+{
+    static void Main(string[] args)
+    {
+        string name = Console.ReadLine();
+
+        double mark = 0.0;
+        int grade = 1;
+        double sum = 0.0;
+
+        while (grade <= 12)
+        {
+
+            mark = double.Parse(Console.ReadLine());
+            sum += mark;
+
+            if (mark < 4)
+            {
+                Console.WriteLine($"{name} has been excluded at {grade} grade");
+                return;
+            }
+            grade++;
+        }
+        double averageGrade = sum / 12;
+        Console.WriteLine($"{name} graduated. Average grade: {averageGrade:f2}");
+    }
+}
