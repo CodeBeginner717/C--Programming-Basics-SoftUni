e7d1f7cf9166d1c21f8fcdac248963ef476f9497
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 19:23:57 +0300

    Add files via upload

---
+++ First Steps in Coding/Exercises/Deposit Calculator/Deposit Calculator/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Deposit_Calculator
+{
+    static void Main(String[] args)
+    {
+
+        long depositSum = long.Parse(Console.ReadLine());
+        int depositPeriod = int.Parse(Console.ReadLine());
+        double annualInterestRate = double.Parse(Console.ReadLine());
+
+        double sum1 = depositSum + (depositPeriod * ((depositSum * annualInterestRate / 100) / 12));
+
+        Console.WriteLine(sum1);
+    }
+}
