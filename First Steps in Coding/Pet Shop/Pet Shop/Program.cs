ecc458932a83ae836be342fa47402078d2e8b734
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 19:14:19 +0300

    Add files via upload

---
+++ First Steps in Coding/Pet Shop/Pet Shop/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+class Pet_Shop
+{
+    static void Main(string[] args)
+    {
+        int dogFood = int.Parse(Console.ReadLine());
+        int catFood = int.Parse(Console.ReadLine());
+
+        double dogFoodPrice = dogFood * 2.50;
+        double catFoodPrice = catFood * 4.00;
+
+        Console.WriteLine(dogFoodPrice + catFoodPrice + " lv.");
+
+    }
+}
---
+++ First Steps in Coding/Projects Creation/Projects Creation/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+class Projects_Creation
+{
+    static void Main(string[] args)
+    {
+        string architectName = Console.ReadLine();
+        int numberOfProjects = int.Parse(Console.ReadLine());
+        int neededHours = numberOfProjects * 3;
+
+        Console.WriteLine($"The architect {architectName} will need {neededHours} hours to complete {numberOfProjects} project/s.");
+
+    }
+}
