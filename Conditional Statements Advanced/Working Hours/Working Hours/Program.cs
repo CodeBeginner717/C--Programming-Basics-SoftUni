8adc4b6d59c358891c2f2faec6c1cb54493e1e5a
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 19:41:59 +0300

    Add files via upload

---
+++ Conditional Statements Advanced/Working Hours/Working Hours/Program.cs
@@ -0,0 +1,30 @@
+﻿class Working_Hours
+{
+    static void Main(string[] args)
+    {
+        int hour = int.Parse(Console.ReadLine());
+        string day = Console.ReadLine();
+
+        if (hour >= 10 && hour <= 18)
+        {
+            switch (day)
+            {
+                case "Monday":
+                case "Tuesday":
+                case "Wednesday":
+                case "Thursday":
+                case "Friday":
+                case "Saturday":
+                    Console.WriteLine("open");
+                    break;
+                default:
+                    Console.WriteLine("closed");
+                    break;
+            }
+        }
+        else if (hour < 10 || hour > 18)
+        {
+            Console.WriteLine("closed");
+        }
+    }
+}
