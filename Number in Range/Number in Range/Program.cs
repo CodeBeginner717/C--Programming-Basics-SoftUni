49d8a503ba399d222e127accc244d20721699eed
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 18:26:51 +0300

    Add files via upload

---
+++ Number in Range/Number in Range/Program.cs
@@ -0,0 +1,17 @@
+﻿class Number_in_Range
+{
+    static void Main(string[] args)
+    {
+        int number = int.Parse(Console.ReadLine());
+
+        if (number == 0 || number > 100 || number < -100)
+        {
+            Console.WriteLine("No");
+        }
+        else
+        {
+            Console.WriteLine("Yes");
+        }
+
+    }
+}
