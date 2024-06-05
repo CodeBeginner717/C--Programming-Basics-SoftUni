db5898ad29cf1b1da602e11c00fd15881fb2a496
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 19:25:18 +0300

    Add files via upload

---
+++ First Steps in Coding/Exercises/Basketball Equipment/Basketball Equipment/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+class Basketball_Equipment
+{
+    static void Main(String[] args)
+    {
+        int annualTaxes = int.Parse(Console.ReadLine());
+
+        double priceOfBasketballShoes = annualTaxes * 0.40;
+        priceOfBasketballShoes = annualTaxes - priceOfBasketballShoes;
+
+        double priceOfBasketballOutfit = priceOfBasketballShoes * 0.2;
+        priceOfBasketballOutfit = priceOfBasketballShoes - priceOfBasketballOutfit;
+
+        double priceOfBall = priceOfBasketballOutfit * 0.25;
+
+        double priceOfAccessories = priceOfBall * 0.20;
+
+        double totalPriceOfOutfit = annualTaxes + priceOfBasketballShoes + priceOfBasketballOutfit + priceOfBall + priceOfAccessories;
+
+        Console.WriteLine(totalPriceOfOutfit);
+    }
+}
---
+++ First Steps in Coding/Exercises/Repainting/Repainting/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+
+class Repainting
+{
+    static void Main(String[] args)
+    {
+        int amountOfNylon = int.Parse(Console.ReadLine());
+        int amountOfPaint = int.Parse(Console.ReadLine());
+        int amountOfThinnerPaint = int.Parse(Console.ReadLine());
+        int hoursOfWork = int.Parse(Console.ReadLine());
+
+        double protectiveNylonPrice = (amountOfNylon + 2) * 1.50;
+        double paintPrice = (amountOfPaint + 1.1) * 14.50;
+        double thinnerPaintPrice = amountOfThinnerPaint * 5;
+        double sumForBags = 0.4;
+
+        double totalAmountForMaterials = protectiveNylonPrice + paintPrice + thinnerPaintPrice + sumForBags;
+        double totalAmountForBuilders = (totalAmountForMaterials * 0.3) * hoursOfWork;
+
+        double finalSum = totalAmountForMaterials + totalAmountForBuilders;
+
+        Console.WriteLine(finalSum);
+    }
+}
