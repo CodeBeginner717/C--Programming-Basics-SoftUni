150388d72e1ac507f94d166c62069386c9c7d652
Daniel <120733908+CodeBeginner717@users.noreply.github.com>
2024-06-05 19:58:59 +0300

    Add files via upload

---
+++ For Loop/Exercises/Tennis Ranklist/Tennis Ranklist/Program.cs
@@ -0,0 +1,39 @@
+﻿class Tennis_Ranklist
+{
+    static void Main()
+    {
+        int tournamentsCount = int.Parse(Console.ReadLine());
+        int startingPoints = int.Parse(Console.ReadLine());
+
+        int finalPoints = startingPoints;
+        int totalPoints = 0;
+        int winsCount = 0;
+
+        for (int i = 0; i < tournamentsCount; i++)
+        {
+            string tournamentResult = Console.ReadLine();
+
+            switch (tournamentResult)
+            {
+                case "W":
+                    finalPoints += 2000;
+                    winsCount++;
+                    break;
+                case "F":
+                    finalPoints += 1200;
+                    break;
+                case "SF":
+                    finalPoints += 720;
+                    break;
+            }
+        }
+
+        totalPoints = finalPoints - startingPoints;
+        double averagePoints = Math.Floor((double)totalPoints / tournamentsCount);
+        double winPercentage = (double)winsCount / tournamentsCount * 100;
+
+        Console.WriteLine($"Final points: {finalPoints}");
+        Console.WriteLine($"Average points: {averagePoints}");
+        Console.WriteLine($"{winPercentage:f2}%");
+    }
+}
